d9a90125d177b84e6cb5ca53ff4e15b357c349c8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-14 12:12:30 -0500

    Feat: advanced article summary endpoint connected

---
+++ app/services/tldr_service.rb
@@ -0,0 +1,23 @@
+class TldrService
+
+  def self.advanced_article_summary(article_url)
+    conn = Faraday.new(url: 'https://tldrthis.p.rapidapi.com/v1/model') do |faraday|
+      faraday.headers['X-RapidAPI-Host'] = 'tldrthis.p.rapidapi.com'
+      faraday.headers['X-RapidAPI-Key'] = ENV['tldr_key']
+      faraday.adapter Faraday.default_adapter
+    end 
+
+    response = conn.post('https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/') do |req|
+      req.body = "{
+                          \"url\": \"#{article_url}\",
+                          \"min_length\": 100,
+                          \"max_length\": 300,
+                          \"is_detailed\": false
+                          }"
+    end
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  
+end
