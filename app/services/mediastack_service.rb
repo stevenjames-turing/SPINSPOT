86e885c4fa933eb9f58139a92e3010f9477c2911
Blake Dempsey <bdempsey864@gmail.com>
2022-04-14 20:39:53 -0400

    Clean up service

--- app/services/mediastack_service.rb
+++ app/services/mediastack_service.rb
@@ -1,11 +1,4 @@
 class MediastackService
-  # def self.conn
-  #   Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
-  #     faraday.params['access_key'] = ENV['mediastack_key']
-  #     faraday.adapter Faraday.default_adapter
-  #   end
-  # end
-
   def self.search_keyword(keyword)
     conn = Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
       faraday.params['access_key'] = ENV['mediastack_key']
