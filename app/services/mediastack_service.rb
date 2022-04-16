1660be1ec8752ae18a40380749e6ef228ce8bec9
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-16 12:05:45 -0500

    Add API params for sources and sorting

--- app/services/mediastack_service.rb
+++ app/services/mediastack_service.rb
@@ -1,9 +1,26 @@
 class MediastackService
+  # source options: 
+                  # ABC News (abc-news), 
+                  # Bloomberg (bloomberg), 
+                  # Cnbc (cnbc), 
+                  # CNN (cnn), 
+                  # Fox News (foxnews),
+                  # Guardian (guardian)
+                  # New York Times (nytimes), 
+                  # NPR (npr-national-public-radio)
+                  # Reuters (reuters), 
+                  # The Verge (the-verge), 
+                  # Time Magazine (time)
+                  # Washington Post (washington-post): Correct source code, but no results returned from API currently
+
   def self.search_keyword(keyword)
     conn = Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
       faraday.params['access_key'] = ENV['mediastack_key']
       faraday.params['keywords'] = "#{keyword}"
+      faraday.params['sources'] = "abc-news,bloomberg,cnbc,cnn,foxnews,guardian,nytimes,npr-national-public-radio,reuters"
       faraday.params['languages'] = "en"
+      faraday.params['countries'] = "us"
+      faraday.params['sort'] = 'published_desc'
       faraday.adapter Faraday.default_adapter
     end
     
