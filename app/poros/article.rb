8babb9419a0eb21be5d5f50ed077867c344e41f0
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-16 14:01:51 -0500

    Refactor: change image to photo_url

--- app/poros/article.rb
+++ app/poros/article.rb
@@ -1,12 +1,12 @@
 class Article 
-  attr_reader :bias, :title, :url, :source, :image, :published_at
+  attr_reader :bias, :title, :url, :source, :photo_url, :published_at
 
   def initialize(article_data, bias)
     @bias = bias 
     @title = article_data[:title]
     @url = article_data[:url]
     @source = article_data[:source]
-    @image = article_data[:image]
+    @photo_url = article_data[:image]
     @published_at = article_data[:published_at]
   end
 end
