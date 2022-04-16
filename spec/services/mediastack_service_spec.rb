6fe166ffc95001b890c80dde6cad9b9e06040d0a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-16 13:00:40 -0500

    Test: center_bias_keyword_search returns articles sourced from center bias publishers

--- spec/services/mediastack_service_spec.rb
+++ spec/services/mediastack_service_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe MediastackService, :vcr do
       end
     end
   end
+
+  context '#center_bias_keyword_search(keyword)' do
+    it 'can find articles from sources with center bias by keyword' do
+      article_url = "http://api.mediastack.com/v1/news"
+      articles = MediastackService.center_bias_keyword_search("elon musk")
+
+      expect(articles).to be_a Hash
+      articles[:data].each do |article|
+        expect(article[:source]).to include("Bloomberg") | include("CNBC") | include("Cnbc") | include("Forbes") | include("NPR") | include("Reuters")
+      end
+    end
+  end
 end
