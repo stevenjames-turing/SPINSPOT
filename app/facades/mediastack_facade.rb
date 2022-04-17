70995baa479cb2b5eef64b1c9faabd3e29b1a530
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-17 16:33:47 -0500

    feat: mediastack facade return single object

--- app/facades/mediastack_facade.rb
+++ app/facades/mediastack_facade.rb
@@ -1,27 +1,17 @@
 class MediastackFacade
 
   def self.left_bias_keyword_search(keyword)
-    json = MediastackService.left_bias_keyword_search(keyword)
-    
-    @articles = json[:data].map do |article_data|
-      Article.new(article_data, "left_bias")
-    end
-  end
-  
-  def self.center_bias_keyword_search(keyword)
-    json = MediastackService.center_bias_keyword_search(keyword)
-    
-    @articles = json[:data].map do |article_data|
-      Article.new(article_data, "center_bias")
-    end
-  end
-  
-  def self.right_bias_keyword_search(keyword)
-    json = MediastackService.right_bias_keyword_search(keyword)
-    
-    @articles = json[:data].map do |article_data|
-      Article.new(article_data, "right_bias")
-    end
+    data = MediastackService.left_bias_keyword_search(keyword)
+    @article = Article.new(data)
   end
 
+  def self.center_bias_keyword_search(keyword)
+    data = MediastackService.center_bias_keyword_search(keyword)
+    @article = Article.new(data)
+  end
+
+  def self.right_bias_keyword_search(keyword)
+    data = MediastackService.right_bias_keyword_search(keyword)
+    @article = Article.new(data)
+  end
 end
--- app/poros/article.rb
+++ app/poros/article.rb
@@ -1,12 +1,11 @@
-class Article 
+class Article
   attr_reader :bias, :title, :url, :source, :photo_url, :published_at
 
-  def initialize(article_data, bias)
-    @bias = bias 
-    @title = article_data[:title]
-    @url = article_data[:url]
-    @source = article_data[:source]
-    @photo_url = article_data[:image]
-    @published_at = article_data[:published_at]
+  def initialize(article_data)
+    @title = article_data[:data][0][:title]
+    @url = article_data[:data][0][:url]
+    @source = article_data[:data][0][:source]
+    @photo_url = article_data[:data][0][:image]
+    @published_at = article_data[:data][0][:published_at]
   end
 end
