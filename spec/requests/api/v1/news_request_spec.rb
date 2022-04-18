33d2d05a5085120bd69656ac2f8a02d076715aba
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-18 17:47:06 -0500

    Test: news API returns json matching contract

--- spec/requests/api/v1/news_request_spec.rb
+++ spec/requests/api/v1/news_request_spec.rb
@@ -2,22 +2,20 @@ require 'rails_helper'
 
 describe 'News API', :vcr do
   context 'news#index' do
-    it 'sends a hash of Tldr objects' do
+    it 'sends a hash of Tldr summaries' do
       
       get '/api/v1/news?keyword=Elon musk'
       
       expect(response).to be_successful
 
-      tldrs = JSON.parse(response.body, symbolize_names: true)
-
-      expect(tldrs).to be_an Array
-
-      expect(tldrs.count).to eq(3)
+      json = JSON.parse(response.body, symbolize_names: true)
       
-      # THERE WILL NEED TO BE MORE TESTS HERE WHEN WE GET THE SERIALIZER SET UP
-      # these tests above will fail when we set up the serializer
+      expect(json).to be_a Hash
       
+      expect(json.keys).to eq([:topic, :type, :data])
       
+      expect(json[:data]).to be_a Hash
+      expect(json[:data].keys).to eq([:left_article, :center_article, :right_article])
     end
   end
 end 
