a582793c671af3fe570eb246b39ab0e5c841b0f2
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-17 20:16:30 -0500

    refactor Mediastach facade

--- app/facades/mediastack_facade.rb
+++ app/facades/mediastack_facade.rb
@@ -2,17 +2,17 @@ class MediastackFacade
 
   def self.left_bias_keyword_search(keyword)
     data = MediastackService.left_bias_keyword_search(keyword)
-    @article = Article.new(data)
+    article = Article.new(data)
   end
 
   def self.center_bias_keyword_search(keyword)
     data = MediastackService.center_bias_keyword_search(keyword)
-    @article = Article.new(data)
+    article = Article.new(data)
   end
 
   def self.right_bias_keyword_search(keyword)
     data = MediastackService.right_bias_keyword_search(keyword)
-    @article = Article.new(data)
+    article = Article.new(data)
   end
 
 end
