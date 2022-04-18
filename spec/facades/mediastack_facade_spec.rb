f0921e55785f3e7bb8c31a7fddfff3b392573713
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-17 20:32:51 -0500

    test: Mediastach Facade

--- spec/facades/mediastack_facade_spec.rb
+++ spec/facades/mediastack_facade_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe MediastackFacade, :vcr do 
+RSpec.describe MediastackFacade, :vcr do
   context 'class methods' do
-    context '#left_bias_keyword_search(keyword)' do 
-      before(:each) do 
-        @left_bias = MediastackFacade.left_bias_keyword_search("Elon")
-      end
-      it 'returns an array of articles from left bias source' do 
-        expect(@left_bias).to be_an Array
-      end
-      it 'creates Article objects' do 
-        expect(@left_bias.first).to be_a Article
-      end
-    end
-
-    context '#center_bias_keyword_search(keyword)' do 
-      before(:each) do 
-        @center_bias = MediastackFacade.center_bias_keyword_search("Elon")
-      end
-      it 'returns an array of articles from left bias source' do 
-        expect(@center_bias).to be_an Array
-      end
-      it 'creates Article objects' do 
-        expect(@center_bias.first).to be_a Article
+    context '#left_bias_keyword_search(keyword)' do
+      it 'creates Article object with attributes' do
+        left_bias = MediastackFacade.left_bias_keyword_search("Elon")
+        expect(left_bias).to be_a Article
+        expect(left_bias.title).to_not be_nil
+        expect(left_bias.url).to_not be_nil
+        expect(left_bias.source).to_not be_nil
+        expect(left_bias.photo_url).to_not be_nil
+        expect(left_bias.published_at).to_not be_nil
       end
     end
 
-    context '#right_bias_keyword_search(keyword)' do 
-      before(:each) do 
-        @right_bias = MediastackFacade.right_bias_keyword_search("Elon")
+    context '#center_bias_keyword_search(keyword)' do
+      it 'creates Article object with attributes' do
+        center_bias = MediastackFacade.center_bias_keyword_search("Elon")
+        expect(center_bias).to be_a Article
+        expect(center_bias).to be_a Article
+        expect(center_bias.title).to_not be_nil
+        expect(center_bias.url).to_not be_nil
+        expect(center_bias.source).to_not be_nil
+        expect(center_bias.photo_url).to_not be_nil
+        expect(center_bias.published_at).to_not be_nil
       end
-      it 'returns an array of articles from left bias source' do 
-        expect(@right_bias).to be_an Array
-      end
-      it 'creates Article objects' do 
-        expect(@right_bias.first).to be_a Article
-      end
+    end
+  end
+
+  context '#right_bias_keyword_search(keyword)' do
+    it 'creates Article objects' do
+      right_bias = MediastackFacade.right_bias_keyword_search("Elon")
+      expect(right_bias).to be_a Article
+      expect(right_bias.title).to_not be_nil
+      expect(right_bias.url).to_not be_nil
+      expect(right_bias.source).to_not be_nil
+      expect(right_bias.photo_url).to_not be_nil
+      expect(right_bias.published_at).to_not be_nil
     end
   end
 end
