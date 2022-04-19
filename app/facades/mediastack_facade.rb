2745b46ba5e8a3a8a093ddf60cb9409989af5c3b
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-19 13:42:50 -0600

    refactor MediastackService/MediastackFacade, DRY

--- app/facades/mediastack_facade.rb
+++ app/facades/mediastack_facade.rb
@@ -1,26 +1,10 @@
 class MediastackFacade
 
-  def self.left_bias_keyword_search(keyword)
-    json = MediastackService.left_bias_keyword_search(keyword)
-    
+  def self.keyword_search(keyword, bias_string)
+    json = MediastackService.keyword_search(keyword, bias_string)
+
     @articles = json[:data].map do |article_data|
-      Article.new(article_data, "left_bias")
-    end
-  end
-  
-  def self.center_bias_keyword_search(keyword)
-    json = MediastackService.center_bias_keyword_search(keyword)
-    
-    @articles = json[:data].map do |article_data|
-      Article.new(article_data, "center_bias")
-    end
-  end
-  
-  def self.right_bias_keyword_search(keyword)
-    json = MediastackService.right_bias_keyword_search(keyword)
-    
-    @articles = json[:data].map do |article_data|
-      Article.new(article_data, "right_bias")
+      Article.new(article_data, bias_string)
     end
   end
 
--- app/services/mediastack_service.rb
+++ app/services/mediastack_service.rb
@@ -1,62 +1,46 @@
 class MediastackService
-  # source options: 
-                  # ABC News (abc-news), 
-                  # Bloomberg (bloomberg), 
-                  # Breitbart (breitbart), 
-                  # Cnbc (cnbc), 
-                  # CNN (cnn), 
+  # source options:
+                  # ABC News (abc-news),
+                  # Bloomberg (bloomberg),
+                  # Breitbart (breitbart),
+                  # Cnbc (cnbc),
+                  # CNN (cnn),
                   # Forbes (forbes),
                   # Fox News (foxnews),
                   # Guardian (guardian)
-                  # New York Times (nytimes), 
+                  # New York Times (nytimes),
                   # NPR (npr-national-public-radio)
-                  # Reuters (reuters), 
-                  # The Verge (the-verge), 
+                  # Reuters (reuters),
+                  # The Verge (the-verge),
                   # Time Magazine (time)
                   # Washington Post (washington-post): Correct source code, but no results returned from API currently
 
-  def self.left_bias_keyword_search(keyword)
+  def self.keyword_search(keyword, bias_string)
     conn = Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
       faraday.params['access_key'] = ENV['mediastack_key']
       faraday.params['keywords'] = "#{keyword}"
-      faraday.params['sources'] = "abc-news,cnn,guardian,nytimes"
+
+      case bias_string
+        when "left_bias"
+          faraday.params['sources'] = "abc-news,cnn,guardian,nytimes"
+        when "center_bias"
+          faraday.params['sources'] = "bloomberg,cnbc,forbes,npr-national-public-radio,reuters"
+        when "right_bias"
+          faraday.params['sources'] = "foxnews,breitbart"
+        else
+          faraday.params['sources'] = nil
+      end
+
       faraday.params['languages'] = "en"
       faraday.params['countries'] = "us"
       faraday.params['sort'] = 'published_desc'
       faraday.params['limit'] = '1'
       faraday.adapter Faraday.default_adapter
     end
+
     response = conn.get("news")
+
     JSON.parse(response.body, symbolize_names: true)
   end
-  
-  def self.center_bias_keyword_search(keyword)
-    conn = Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
-      faraday.params['access_key'] = ENV['mediastack_key']
-      faraday.params['keywords'] = "#{keyword}"
-      faraday.params['sources'] = "bloomberg,cnbc,forbes,npr-national-public-radio,reuters"
-      faraday.params['languages'] = "en"
-      faraday.params['countries'] = "us"
-      faraday.params['sort'] = 'published_desc'
-      faraday.params['limit'] = '1'
-      faraday.adapter Faraday.default_adapter
-    end
-    response = conn.get("news")
-    JSON.parse(response.body, symbolize_names: true)
-  end
-  
-  def self.right_bias_keyword_search(keyword)
-    conn = Faraday.new(url: 'http://api.mediastack.com/v1/') do |faraday|
-      faraday.params['access_key'] = ENV['mediastack_key']
-      faraday.params['keywords'] = "#{keyword}"
-      faraday.params['sources'] = "foxnews,breitbart"
-      faraday.params['languages'] = "en"
-      faraday.params['countries'] = "us"
-      faraday.params['sort'] = 'published_desc'
-      faraday.params['limit'] = '1'
-      faraday.adapter Faraday.default_adapter
-    end
-    response = conn.get("news")
-    JSON.parse(response.body, symbolize_names: true)
-  end
+
 end
