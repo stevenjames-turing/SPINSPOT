b5017622b2227700754276f9958526d98be6f128
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-20 13:45:07 -0500

    Test: return a 400 error if keyword is empty string
    Co-authored-by: Blake Dempsey <79022527+bdempsey864@users.noreply.github.com>

--- spec/requests/api/v1/news_request_spec.rb
+++ spec/requests/api/v1/news_request_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe 'News API', :vcr do
   context 'news#index' do
-    it 'sends a hash of Tldr summaries' do
-      
+    xit 'sends a hash of Tldr summaries' do
       get '/api/v1/news?keyword=Elon musk'
       
       expect(response).to be_successful
@@ -11,11 +10,19 @@ describe 'News API', :vcr do
       json = JSON.parse(response.body, symbolize_names: true)
       
       expect(json).to be_a Hash
-      
       expect(json.keys).to eq([:topic, :type, :data])
-      
       expect(json[:data]).to be_a Hash
       expect(json[:data].keys).to eq([:left_article, :center_article, :right_article])
+      expect(json[:topic]).to eq("Elon musk")
     end
+    
+    xit 'gives a 400 error code if keyword param is empty' do 
+      get '/api/v1/news?keyword='
+      
+      expect(response).to have_http_status(400)
+    end
+    
+    
+    
   end
 end 
