409156e0b6f9dfbae0c24c1d7e67849d3e05b9aa
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-20 13:48:40 -0500

    Refactor: remove variable name from object creation
    Co-authored-by: Blake Dempsey <79022527+bdempsey864@users.noreply.github.com>

--- app/facades/mediastack_facade.rb
+++ app/facades/mediastack_facade.rb
@@ -3,7 +3,7 @@ class MediastackFacade
   def self.keyword_search(keyword, bias_string)
     json = MediastackService.keyword_search(keyword, bias_string)
 
-    @article = Article.new(json[:data][0], bias_string)
+    Article.new(json[:data][0], bias_string)
   end
 
 end
