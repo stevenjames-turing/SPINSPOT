faf9af0e54c7f14f047e0037d449168d37cb790f
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-20 15:24:59 -0500

    Refactor: Update tests to reflect single article vs array of objects

--- spec/facades/tldr_facade_spec.rb
+++ spec/facades/tldr_facade_spec.rb
@@ -4,41 +4,23 @@ RSpec.describe TldrFacade, :vcr do
   context 'class methods' do
     context '#advanced_article_summary(article_url)' do
       before(:each) do
-        @articles = MediastackFacade.keyword_search("Elon", "left_bias")
-        @advanced_summary = TldrFacade.advanced_article_summary(@articles.first.url, @articles.first.bias, @articles.first.source)
+        @article = MediastackFacade.keyword_search("Elon", "left_bias")
+        @advanced_summary = TldrFacade.advanced_article_summary(@article.url, @article.bias, @article.source)
       end
 
       it 'creates a Tldr object for article passed as argument' do
         expect(@advanced_summary).to be_a Tldr
       end
-
-      it 'can create multiple Tldr for all articles' do
-        objects = @articles.map do |article|
-          TldrFacade.advanced_article_summary(article.url, article.bias, article.source)
-        end
-
-        expect(objects.first).to be_a Tldr
-        expect(objects.count).to eq(@articles.count)
-      end
     end
 
     context '#standard_article_summary(article_url)' do
       before(:each) do
-        @articles = MediastackFacade.keyword_search("Elon", "left_bias")
-        @standard_summary = TldrFacade.standard_article_summary(@articles.first.url, @articles.first.bias, @articles.first.source)
+        @article = MediastackFacade.keyword_search("Elon", "left_bias")
+        @standard_summary = TldrFacade.standard_article_summary(@article.url, @article.bias, @article.source)
       end
 
       it 'creates a Tldr object for article passed as argument' do
         expect(@standard_summary).to be_a Tldr
-      end
-
-      it 'can create multiple Tldr for all articles' do
-        objects = @articles.map do |article|
-          TldrFacade.standard_article_summary(article.url, article.bias, article.source)
-        end
-
-        expect(objects.first).to be_a Tldr
-        expect(objects.count).to eq(@articles.count)
       end
     end
   end
