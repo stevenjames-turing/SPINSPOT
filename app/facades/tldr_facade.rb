6366db31074739d534f971af84e28b12d7554f2f
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-21 17:34:01 -0600

    update ::standard_article_summary to reflect ::advanced_article_summary

--- app/facades/tldr_facade.rb
+++ app/facades/tldr_facade.rb
@@ -20,8 +20,20 @@ class TldrFacade
   end
 
   def self.standard_article_summary(article_url, bias, source)
-    json = TldrService.standard_article_summary(article_url)
+    if article_url == nil
 
-    @tldr = Tldr.new(json, bias, source)
+      nil_response = {
+        article_title: "Article Not Found!",
+        summary: ["Whatever you were looking up is probably fake news anyway."],
+        url: nil,
+        article_image: "https://stryvemarketing.com/wp-content/uploads/2016/04/image.gif",
+        pub_date: nil
+      }
+
+      @tldr = Tldr.new(nil_response, bias, source)
+    else
+      json = TldrService.standard_article_summary(article_url)
+      @tldr = Tldr.new(json, bias, source)
+    end
   end
 end
