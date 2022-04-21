f07fa2d6285dbab45ba7ccfad58a5073abad680c
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-21 16:25:35 -0600

    add control flow for bad results

--- app/facades/mediastack_facade.rb
+++ app/facades/mediastack_facade.rb
@@ -3,7 +3,19 @@ class MediastackFacade
   def self.keyword_search(keyword, bias_string)
     json = MediastackService.keyword_search(keyword, bias_string)
 
-    Article.new(json[:data][0], bias_string)
+    if json[:data] == []
+      empty_response = {
+        title: "No Articles Found!",
+        url: nil,
+        source: nil,
+        photo_url: nil,
+        publish_at: nil
+      }
+
+      Article.new(empty_response, bias_string)
+    else
+      Article.new(json[:data][0], bias_string)
+    end
   end
 
 end
